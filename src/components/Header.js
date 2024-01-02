350683452f79e961889911f458af426bc3ed485c
hassan <hassanak509@gmail.com>
2024-01-02 22:22:44 +0200

    add components

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from './Nav'
+
+export default function Header() {
+    return (
+        <header>
+            <img src="./images/little-lemon-logo.png" alt="little lemon logo"/>
+            <Nav />
+        </header>
+    )
+}
+
---
+++ src/components/Nav.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <a href='' role='button' >Home</a>
+            </li>
+            <li>
+                <a href='' role='button' >About</a>
+            </li>
+            <li>
+                <a href='' role='button' >Menu</a>
+            </li>
+            <li>
+                <a href='' role='button' >Reservations</a>
+            </li>
+            <li>
+                <a href='' role='button' >Order Online</a>
+            </li>
+            <li>
+                <a href='' role='button' >Login</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
