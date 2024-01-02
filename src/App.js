c1e8931a0ff9f81894dec1d52022745b769289c6
hassan <hassanak509@gmail.com>
2024-01-02 22:23:15 +0200

    add styles

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+// import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      HomePage
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
