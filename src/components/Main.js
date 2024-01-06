99c78184af9641999ff6f07fcaa1ee9af70d6858
hassan <hassanak509@gmail.com>
2024-01-06 14:17:53 +0200

    update components

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Main() {
   return (
-    <main>Main</main>
+    <main>
+      <section className='first-section'></section>
+      <section className='week-special-section'></section>
+      <section className='testimonials-section'></section>
+      <section className='about-section'></section>
+    </main>
   )
 }
