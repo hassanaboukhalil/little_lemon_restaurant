307784caeb3f96f3056d919f5002c11e9811418e
hassan <hassanak509@gmail.com>
2024-01-09 15:22:00 +0200

    use formik and chakra libraries

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,34 +1,67 @@
+import { Button, FormControl, FormLabel, Input, Select } from '@chakra-ui/react'
 import { useFormik } from 'formik'
+// import { useFormik } from 'formik'
 import React from 'react'
+import AvailableTimes from './availableTimes'
+// import AvailableTimes
 
-// const formik = useFormik({
-//     initialValues: {
+function BookingForm({times_state_obj}) {
+    const formik = useFormik({
+        initialValues: {
+            date: new Date().toISOString().split('T')[0],
+            time: "17",
+            guests_nb: 1,
+            occasion: "Birthday"
+        }
+    })
 
-//     }
-// })
-
-function BookingForm() {
     return (
+        // <form className='booking-form'>
+        //     {/* <label htmlFor="res-date">Choose date</label>
+        //     <input type="date" id="res-date" />
+        //     <label htmlFor="res-time">Choose time</label>
+        //     <select id="res-time ">
+        //         <option>17:00</option>
+        //         <option>18:00</option>
+        //         <option>19:00</option>
+        //         <option>20:00</option>
+        //         <option>21:00</option>
+        //         <option>22:00</option>
+        //     </select>
+        //     <label htmlFor="guests">Number of guests</label>
+        //     <input type="number" placeholder="1" min="1" max="10" id="guests" />
+        //     <label htmlFor="occasion">Occasion</label>
+        //     <select id="occasion">
+        //         <option>Birthday</option>
+        //         <option>Anniversary</option>
+        //     </select>
+        //     <input type="submit" value="Make Your reservation" /> */}
+        // </form>
         <form className='booking-form'>
-            <label htmlFor="res-date">Choose date</label>
-            <input type="date" id="res-date" />
-            <label htmlFor="res-time">Choose time</label>
-            <select id="res-time ">
-                <option>17:00</option>
-                <option>18:00</option>
-                <option>19:00</option>
-                <option>20:00</option>
-                <option>21:00</option>
-                <option>22:00</option>
-            </select>
-            <label htmlFor="guests">Number of guests</label>
-            <input type="number" placeholder="1" min="1" max="10" id="guests" />
-            <label htmlFor="occasion">Occasion</label>
-            <select id="occasion">
-                <option>Birthday</option>
-                <option>Anniversary</option>
-            </select>
-            <input type="submit" value="Make Your reservation" />
+            <FormControl >
+                <FormLabel>Choose date</FormLabel>
+                <Input type='date' {...formik.getFieldProps("date")} />
+            </FormControl>
+            <FormControl >
+                <FormLabel>Choose time</FormLabel>
+                <Select {...formik.getFieldProps("time")}>
+                    <AvailableTimes times_state_obj={times_state_obj}/>
+                </Select>
+            </FormControl>
+            <FormControl >
+                <FormLabel>Number of guests</FormLabel>
+                <Input type="number" min="1" max="10" {...formik.getFieldProps("guests_nb")}/>
+            </FormControl>
+            <FormControl >
+                <FormLabel>Occasion</FormLabel>
+                <Select {...formik.getFieldProps("occasion")}>
+                    <option>Birthday</option>
+                    <option>Anniversary</option>
+                </Select>
+            </FormControl>
+            <Button w="100%" bg="#7f5ad5" color="white" isLoading={false}>
+                Submit
+            </Button>
         </form>
     )
 }
