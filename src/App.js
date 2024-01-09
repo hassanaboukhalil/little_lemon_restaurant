a6b522a921163ac897d96764aca9b4326dba1031
hassan <hassanak509@gmail.com>
2024-01-09 15:23:28 +0200

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import './css/App.css';
 import HomePage from './components/HomePage';
 import BookingPage from './components/BookingPage';
 import { Route, Routes } from 'react-router-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 // import Nav from './components/Nav';
+
+
 
 function App() {
   return (
-    <>
-      <Routes>
-        <Route path='/' element={<HomePage />} />
-        <Route path='/booking-page' element={<BookingPage />} />
-      </Routes>
-    </>
+    // <>
+    //   <Routes>
+    //       <Route path='/' element={<HomePage />} />
+    //       <Route path='/booking-page' element={<BookingPage />} />
+    //   </Routes>
+    // </>
+    <ChakraProvider>
+        <Routes>
+          <Route path='/' element={<HomePage />} />
+          <Route path='/booking-page' element={<BookingPage />} />
+        </Routes>
+    </ChakraProvider>
   );
 }
 
