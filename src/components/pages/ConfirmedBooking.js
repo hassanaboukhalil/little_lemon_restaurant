4aeb508c932edc23b975b007d4809b0e4ae754e5
hassan <hassanak509@gmail.com>
2024-01-13 17:14:00 +0200

    add ConfirmedBooking.js component

---
+++ src/components/pages/ConfirmedBooking.js
@@ -0,0 +1,11 @@
+import Main from "../layout/Main";
+
+export default function ConfirmedBooking() {
+    return (
+        <Main>
+            <p className="title confirmed-title">Booking Confirmed!!!</p>
+            <p className="subtitle confirmed-subtitle">Please provide your name and contact number at the reception upon arrival</p>
+            <p className="subtitle confirmed-subtitle">We are excited to serve you</p>
+        </Main>
+    )
+}
