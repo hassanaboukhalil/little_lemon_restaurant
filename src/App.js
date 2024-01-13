ada56b81ca4369dfc61f20edb0c3cfd731f65d3d
hassan <hassanak509@gmail.com>
2024-01-13 17:12:24 +0200

    update App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import './css/App.css';
-import HomePage from './components/HomePage';
-import BookingPage from './components/BookingPage';
+import HomePage from './components/pages/HomePage';
+import BookingPage from './components/pages/BookingPage';
 import { Route, Routes } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
+import ConfirmedBooking from './components/pages/ConfirmedBooking';
 // import Nav from './components/Nav';
 
 
@@ -16,10 +19,13 @@ function App() {
     //   </Routes>
     // </>
     <ChakraProvider>
+        <Header />
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='/booking-page' element={<BookingPage />} />
+          <Route path='/confirmed-booking' element={<ConfirmedBooking />} />
         </Routes>
+        <Footer />
     </ChakraProvider>
   );
 }
