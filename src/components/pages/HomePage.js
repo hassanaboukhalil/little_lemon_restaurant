ff34ca0317c1cbff1de24ce5536ae1c7cb577ec9
hassan <hassanak509@gmail.com>
2024-01-30 10:52:22 +0200

    update the home page component

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import Main from '../layout/Main'
+import Hero from '../others/Hero'
+import Highlights from '../others/Highlights'
 
 function HomePage() {
   return (
     <>
         <Main>
-          {/* <section className='first-section'></section>
-          <section className='week-special-section'></section>
-          <section className='testimonials-section'></section>
-          <section className='about-section'></section> */}
-          <p>hii</p>
+          <Hero />
+          <Highlights />
         </Main>
     </>
   )
