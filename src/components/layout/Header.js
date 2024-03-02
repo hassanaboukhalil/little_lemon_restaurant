bacc803ff5824f05f69ac862838a3af3f7a69ee1
hassan <hassanak509@gmail.com>
2024-03-02 15:34:57 +0200

    update the nav

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,9 +7,9 @@ export default function Header() {
     return (
         <header>
             <div className='container'>
-                <Link to='/' className='nav-item' aria-label="On Click">
+                {/* <Link to='/' className='nav-item' aria-label="On Click">
                     <img src="./images/little-lemon-logo.png" alt="little lemon logo"/>
-                </Link>
+                </Link> */}
                 <Nav />
             </div>
         </header>
--- src/components/layout/Nav.js
+++ src/components/layout/Nav.js
@@ -4,18 +4,19 @@ import { Link } from 'react-router-dom'
 export default function Nav() {
     let navRef = useRef(null)
     let besideNavRef = useRef(null)
+    let logo = useRef(null)
     let navHome = useRef(null)
     let navAbout = useRef(null)
     let navReservations = useRef(null)
 
     function openNav(){
-        besideNavRef.style.display = 'unset';
-        navRef.style.display = 'unset';
+        besideNavRef.current.style.display = 'unset';
+        navRef.current.style.display = 'unset';
     }
 
     function closeNav(){
-        besideNavRef.style.display = 'none';
-        navRef.style.display = 'none';
+        besideNavRef.current.style.display = 'none';
+        navRef.current.style.display = 'none';
     }
 
     useEffect(() => {
@@ -37,21 +38,28 @@ export default function Nav() {
                 arr[i].style.fontWeight = "unset";
                 arr[i].style.padding = "unset";
             }
+            if(!window.matchMedia("(min-width: 992px)").matches){
+                closeNav()
+            }
         }
         navHome.current.addEventListener('click', () => { changeNav(0) });
         navAbout.current.addEventListener('click', () => {
-            changeNav(0) ;
+            changeNav(0)
             setTimeout(() => {
                 document.getElementById('About').scrollIntoView({ behavior: 'smooth' });
             }, 100);
         });
         navReservations.current.addEventListener('click', () => { changeNav(2) });
         changeNav(0)
+        logo.current.addEventListener('click', () => { changeNav(0) });
     },[])
 
     return (
         window.matchMedia("(min-width: 992px)").matches ?
         <>
+            <Link to='/' ref={logo} className='nav-item' aria-label="On Click">
+                <img src="./images/little-lemon-logo.png" alt="little lemon logo"/>
+            </Link>
             <nav role='navigation' ref={navRef}>
                 <ul>
                     <li>
@@ -77,6 +85,9 @@ export default function Nav() {
         </>
         :
         <>
+            <Link to='/' ref={logo} className='nav-item' aria-label="On Click">
+                <img src="./images/little-lemon-logo.png" alt="little lemon logo"/>
+            </Link>
             <svg className='hamburger-icon' onClick={openNav} width="35" height="27" viewBox="0 0 35 27" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd" d="M1.34615 0C0.6027 0 0 0.652724 0 1.45788C0 2.26304 0.6027 2.91577 1.34615 2.91577H33.6538C34.3972 2.91577 35 2.26304 35 1.45788C35 0.652724 34.3972 0 33.6538 0H1.34615ZM0 13.121C0 12.3158 0.6027 11.6631 1.34615 11.6631H33.6538C34.3972 11.6631 35 12.3158 35 13.121C35 13.9261 34.3972 14.5788 33.6538 14.5788H1.34615C0.6027 14.5788 0 13.9261 0 13.121ZM0 24.784C0 23.979 0.6027 23.3261 1.34615 23.3261H33.6538C34.3972 23.3261 35 23.979 35 24.784C35 25.5891 34.3972 26.2419 33.6538 26.2419H1.34615C0.6027 26.2419 0 25.5891 0 24.784Z" fill="black"/>
             </svg>
