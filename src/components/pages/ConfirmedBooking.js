a03a039b8e07cb62ef82e1efa111f5745fcaf8e4
hassan <hassanak509@gmail.com>
2024-03-06 09:01:12 +0200

    make some changes

--- src/components/pages/ConfirmedBooking.js
+++ src/components/pages/ConfirmedBooking.js
@@ -8,7 +8,7 @@ export default function ConfirmedBooking() {
             <section className="confirmed-booking">
                 <div className="container">
                     <p className="title">Booking Confirmed!!!</p>
-                    <p className="subtitle">Please provide your name and contact number at the reception upon arrival</p>
+                    <p className="subtitle">Please provide your name and contact number or email address at the reception upon arrival</p>
                     <p className="subtitle">We are excited to serve you</p>
                 </div>
             </section>
