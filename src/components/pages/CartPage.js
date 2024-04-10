aec57db357c167ce7a0f84c330b171841b85bc38
hassan <hassanak509@gmail.com>
2024-04-10 23:46:00 +0300

    add an dialog for payment successful

--- src/components/pages/CartPage.js
+++ src/components/pages/CartPage.js
@@ -1,17 +1,29 @@
 import React, { useEffect } from 'react'
 import Main from '../layout/Main'
-import { Button, FormControl, Img, Input } from '@chakra-ui/react'
+import { AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay, AlertDialogCloseButton, Button, FormControl, Img, Input, useDisclosure, Alert, AlertIcon, AlertTitle, AlertDescription, CloseButton } from '@chakra-ui/react'
 import '../../css/cart.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCartData } from '../../store/cartSlice';
+import { useNavigate } from 'react-router-dom'
+
+
 
 
 export default function CartPage() {
   const dispatch = useDispatch()
   const cart = useSelector((state) => state.cart);
+  let navigate = useNavigate()
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const cancelRef = React.useRef()
+
   useEffect(() => {
     dispatch(fetchCartData());
   }, [dispatch]);
+
+  const closeAlert = () => {
+    onClose()
+    navigate("/")
+  }
 
 
   return (
@@ -74,9 +86,28 @@ export default function CartPage() {
                       <p>58$</p>
                     </div>
                   </div>
-                  <Button className='btn' type="submit" w="100%" bg="#7f5ad5" isLoading={false} aria-label="On Click">
+                  <Button className='btn' type="submit" w="100%" bg="#7f5ad5" isLoading={false} aria-label="On Click" onClick={onOpen}>
                     Pay For Order
                   </Button>
+
+                  <AlertDialog
+                    motionPreset='slideInBottom'
+                    leastDestructiveRef={cancelRef}
+                    onClose={onClose}
+                    isOpen={isOpen}
+                    isCentered
+                  >
+                    <AlertDialogOverlay />
+
+                    <AlertDialogContent bg='#9AE6B4'>
+                      <AlertDialogHeader>Success!</AlertDialogHeader>
+                      <AlertDialogCloseButton onClick={closeAlert} />
+                      <AlertDialogBody>
+                        The payment is successful. The order will be deliverd soon
+                      </AlertDialogBody>
+                    </AlertDialogContent>
+                  </AlertDialog>
+                  
                 </div>
             </div>
         </section>
