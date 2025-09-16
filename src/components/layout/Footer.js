016abe30929574b06e69c27a6166a5f45fc1c76d
hassan <hassanak509@gmail.com>
2025-09-16 08:01:02 +0300

    refactor Footer component: add navigation links, improve structure, and enhance social media section

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,39 +1,80 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Footer() {
   return (
     <footer>
-        <div className='container'>
-          <img src='./images/logo2.png' alt='logo2' />
-          <div className='texts-and-links'>
-            <div>
-              <h5>Doormat Navigation</h5>
-              <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Order Online</li>
-                <li>Reservation</li>
-                <li>Login</li>
-              </ul>
-            </div>
-            <div>
-              <h5>Contact</h5>
-              <ul>
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-              </ul>
-            </div>
-            <div>
-              <h5>Social Media Links</h5>
-              <ul>
-                <li>Address</li>
-                <li>Phone Number</li>
-                <li>Email</li>
-              </ul>
-            </div>
+      <div className="container">
+        <img src="./images/logo2.png" alt="logo2" />
+        <div className="texts-and-links">
+          <div>
+            <h5>Doormat Navigation</h5>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/menu">Order Online</Link>
+              </li>
+              <li>
+                <Link to="/booking">Reservation</Link>
+              </li>
+              <li>
+                <Link to="/login">Login</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Contact</h5>
+            <ul>
+              <li>
+                <Link to="/contact">Address</Link>
+              </li>
+              <li>
+                <Link to="/contact">Phone Number</Link>
+              </li>
+              <li>
+                <Link to="/contact">Email</Link>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <h5>Social Media Links</h5>
+            <ul>
+              <li>
+                <a
+                  href="https://facebook.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Facebook
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://instagram.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://twitter.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Twitter
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
+      </div>
     </footer>
-  )
+  );
 }
