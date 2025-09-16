3574f01e69b94a881f3a980d96fcaee39522ae05
hassan <hassanak509@gmail.com>
2025-09-16 08:10:54 +0300

    refactor FoodCard component: improve code readability and formatting, update link paths

--- src/components/others/FoodCard.js
+++ src/components/others/FoodCard.js
@@ -1,30 +1,32 @@
-import { Button } from '@chakra-ui/react'
-import React from 'react'
-import { Link } from 'react-router-dom'
+import { Button } from "@chakra-ui/react";
+import React from "react";
+import { Link } from "react-router-dom";
 
-function FoodCard({food}) {
+function FoodCard({ food }) {
   return (
-    <div className='food-card' key={food.id}>
-        <div className='texts'>
-            <div className='title-and-price'>
-              <h1>{food.name}</h1>
-              <p className='des price'>{food.price}</p>
-            </div>
-            <p className='des'>
-                {food.des}
-            </p>
-            <div className='btns'>
-              <Button className='order-btn'>
-                <Link to='/contact-page' className='nav-item' aria-label="On Click">Order Now</Link>
-              </Button>
-              <Button className='add-btn'>
-                <Link to='/login-page' className='nav-item' aria-label="On Click">Add To Cart</Link>
-              </Button>
-            </div>
+    <div className="food-card" key={food.id}>
+      <div className="texts">
+        <div className="title-and-price">
+          <h1>{food.name}</h1>
+          <p className="des price">{food.price}</p>
         </div>
-        <img src={food.src} alt={food.name} />
+        <p className="des">{food.des}</p>
+        <div className="btns">
+          <Button className="order-btn">
+            <Link to="/contact-page" className="nav-item" aria-label="On Click">
+              Order Now
+            </Link>
+          </Button>
+          <Button className="add-btn">
+            <Link to="/login" className="nav-item" aria-label="On Click">
+              Add To Cart
+            </Link>
+          </Button>
+        </div>
+      </div>
+      <img src={food.src} alt={food.name} />
     </div>
-  )
+  );
 }
 
-export default FoodCard
+export default FoodCard;
