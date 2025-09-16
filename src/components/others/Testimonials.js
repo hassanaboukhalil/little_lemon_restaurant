46576fdf42f5f401e481dc98c010de5a27e7291a
hassan <hassanak509@gmail.com>
2025-09-16 08:29:18 +0300

    update Testimonials component: add dynamic user data and images for raters

--- src/components/others/Testimonials.js
+++ src/components/others/Testimonials.js
@@ -1,21 +1,49 @@
-import { Center, Grid } from '@chakra-ui/react'
-import React from 'react'
-import Rater from './Rater'
+import { Center, Grid } from "@chakra-ui/react";
+import React from "react";
+import Rater from "./Rater";
+// import image_1 from "../../../public/images/testimonials/rater-1.jpeg";
+// import image_2 from "../../../public/images/testimonials/rater-2.jpeg";
+// import image_3 from "../../../public/images/testimonials/rater-3.jpeg";
+// import image_4 from "../../../public/images/testimonials/rater-4.jpeg";
 
 function Testimonials() {
   return (
-    <section className='section3'>
-      <div className='testimonials container'>
-        <Center className='title' as='b'>Testimonials</Center>
-        <Grid className='raters' templateColumns='auto' paddingX='2rem' spacing='2.94rem' justifyItems='center'>
-            <Rater />
-            <Rater />
-            <Rater />
-            <Rater />
+    <section className="section3">
+      <div className="testimonials container">
+        <Center className="title" as="b">
+          Testimonials
+        </Center>
+        <Grid
+          className="raters"
+          templateColumns="auto"
+          paddingX="2rem"
+          spacing="2.94rem"
+          justifyItems="center"
+        >
+          <Rater
+            firstName="Yousef"
+            lastName="Ferman"
+            image="./images/testimonials/rater-1.jpeg"
+          />
+          <Rater
+            firstName="Mostafa"
+            lastName="Ahmed"
+            image="./images/testimonials/rater-2.jpeg"
+          />
+          <Rater
+            firstName="Kareem"
+            lastName="Mohamed"
+            image="./images/testimonials/rater-3.jpeg"
+          />
+          <Rater
+            firstName="Selena"
+            lastName="Jawad"
+            image="./images/testimonials/rater-4.jpeg"
+          />
         </Grid>
       </div>
     </section>
-  )
+  );
 }
 
-export default Testimonials
+export default Testimonials;
