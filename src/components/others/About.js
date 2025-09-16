a8e4c4c4ca493950b1e4035a349257b5b6302ce1
hassan <hassanak509@gmail.com>
2025-09-16 08:38:16 +0300

    update About component: enhance content with restaurant description and improve code formatting

--- src/components/others/About.js
+++ src/components/others/About.js
@@ -1,22 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function About() {
   return (
-    <section className='section3' id='About'>
-      <div className='about container'>
+    <section className="section3" id="About">
+      <div className="about container">
         <div>
           <h1>Little Lemon</h1>
           <h2>Chicago</h2>
           <p>
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+            Founded in 2010, Little Lemon is a charming family-owned
+            Mediterranean restaurant that brings the flavors of the
+            Mediterranean to the heart of Chicago. Our culinary philosophy
+            centers on traditional recipes prepared with a contemporary twist.
             <br />
-            Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.
+            Our chefs source the freshest ingredients from local markets to
+            create dishes that honor our heritage while delighting modern
+            palates. From our signature Greek salad to our renowned lemon
+            dessert, every item on our menu tells a story of tradition and
+            innovation.
           </p>
         </div>
-        <img src='./images/about.png' alt='two men' />
+        <img src="./images/about.png" alt="two men" />
       </div>
     </section>
-  )
+  );
 }
 
-export default About
+export default About;
